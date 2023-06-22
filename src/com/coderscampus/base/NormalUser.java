fae64190be56bc8af9eada7862915653c741286f
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-06-22 13:45:45 -0500

    added function in SuperUser & NormalUser classes

--- src/com/coderscampus/base/NormalUser.java
+++ src/com/coderscampus/base/NormalUser.java
@@ -1,5 +1,14 @@
 package com.coderscampus.base;
 
 public class NormalUser extends User {
+/**
+ * @param username
+ * @param password
+ */
 
+public NormalUser() {    
+    this.setReadAccess(true);
+    this.setWriteAccess(true);
+    this.setSuperUser(false);   
+  } 
 }
